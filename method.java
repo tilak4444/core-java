520b8c83220fa65e4c55e54ef9620615be2faa04
Your Name <you@example.com>
2024-10-03 10:03:12 +0530

    test

---
+++ method.java
@@ -0,0 +1,13 @@
+public class method{
+public static void main(String[]args){
+System.out.println("what is your name");
+Name("tilak nayak");
+Name("miskin");
+
+
+
+}
+public static void Name(String x){
+System.out.println("my name is "+x);
+}
+}
