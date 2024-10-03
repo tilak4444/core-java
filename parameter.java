574d9e8ff30ed970d6e564c6ecefedaf3c5c53b8
Your Name <you@example.com>
2024-10-03 10:06:42 +0530

    Second task

---
+++ parameter.java
@@ -0,0 +1,9 @@
+public class twoparameters{
+public static void main(String[]args){
+	modulus(10,2);
+}
+public staticvoid modulus(int num1, int num2){
+	int answer = num1 % num2;
+	System.out.println("thw modulus of given number is " + answer);
+}
+}
